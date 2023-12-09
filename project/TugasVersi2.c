b9844fd015445e9875493b03104f08f157dd4e08
Azka Ilham Ramadhani <103100910+AzkaIlham@users.noreply.github.com>
2023-12-09 20:34:52 +0800

    Update TugasVersi2.c
    update code

--- project/TugasVersi2.c
+++ project/TugasVersi2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void menu_customer();
 void genreFilm();
@@ -7,6 +9,14 @@ void komedi();
 void romantis();
 void pemesanan();
 void keluar();
+
+typedef struct{
+    int id;
+    char title[50];
+    char genre[20];
+    int stock;
+    float price;
+}Movie;
 
 void intro(){
 
@@ -30,8 +40,8 @@ void menu_customer(){
     printf("\n\t===============================================");
     printf("\n\t||               Tampilan Menu               ||");
     printf("\n\t===============================================");
-    printf("\n\t||  1. Menu Pemesanan Tiket                  ||");
-    printf("\n\t||  2. Exit                                  ||");
+    printf("\n\t||  1. Menu Pemesanan Tiket                 ||");
+    printf("\n\t||  2. Exit                                 ||");
     printf("\n\t===============================================");
     printf("\n\t Masukkan Pilihan Anda: ");
     scanf("%d", &menu);
@@ -73,32 +83,82 @@ void genreFilm(){
 }
 
 void horor(){
-    printf("\n\tSegera Dibuat");
+    Movie movies[] = {
+        {1, "Conjuring", "Horor", 50, 80000},
+        {2, "Annabelle Creation", "Horor", 30, 85000},
+        {3, "It", "Horor", 20, 90000},
+    };
+    pemesanan(movies, 3);
 }
 
 void komedi(){
-    printf("\n\tSegera Dibuat");
+    Movie movies[] = {
+        {4, "Friends", "Komedi", 100, 60000},
+        {5, "Modern Family", "Komedi", 70, 70000},
+        {6, "Curb Your Enthusiasm", "Komedi", 40, 65000},
+    };
+    pemesanan(movies, 3);
 }
 
 void romantis(){
-    printf("\n\tSegera Dibuat");
+    Movie movies[] = {
+        {7, "Pride and Prejudice", "Romantis", 60, 70000},
+        {8, "Love Actually", "Romantis", 40, 75000},
+        {9, "When Harry Met Sally", "Romantis", 50, 80000},
+    };
+    pemesanan(movies, 3);
 }
 
+void pemesanan(Movie movies[], int jumlah_movie){
+    int id_movie, jumlah_tiket, i;
+    int cari = 0;
+    printf("\n\t==============================");
+    printf("\n\t||   Daftar Film yang Tayang ||");
+    printf("\n\t==============================");
+    printf("\n\t ID | Title               | Genre    | Stock | Price   ");
+    printf("\n\t-----------------------------------------------------------------");
+    for(i=0; i<jumlah_movie; i++){
+        printf("\n\t %d | %s             | %s   |   %d   | Rp. %.2f ",
+        movies[i].id, movies[i].title, movies[i].genre, movies[i].stock, movies[i].price);
+    }
+    printf("\n\t-----------------------------------------------------------------");
+    printf("\n\t Masukkan ID Film yang Anda Pilih: ");
+    scanf("%d", &id_movie);
+
+    for(i=0; i<jumlah_movie; i++){
+        if(movies[i].id == id_movie){
+            cari = 1;
+            printf("\n\t Anda telah memilih film: %s", movies[i].title);
+            printf("\n\t Jumlah Stock Tersedia: %d", movies[i].stock);
+            printf("\n\t Masukkan Jumlah Tiket yang Anda Inginkan: ");
+            scanf("%d", &jumlah_tiket);
+
+            if(jumlah_tiket > movies[i].stock){
+                printf("\n\t Maaf, Jumlah Tiket yang Anda Inginkan tidak tersedia. ");
+            }
+            else{
+                movies[i].stock -= jumlah_tiket;
+                printf("\n\t Berhasil Membeli %d Tiket Film %s. Sisa Stock Tiket: %d ", jumlah_tiket, movies[i].title, movies[i].stock);
+            }
+        }
+    }
+
+    if(cari == 0){
+        printf("\n\t Eror/n");
+    }
+
+    printf("\n\t Untuk Melanjutkan Silahkan Pilih Genre Film ");
+    genreFilm();
+}
 
 void keluar(){
-    printf("\n\t==========================================================");
-    printf("\n\t|                                                        |");
-    printf("\n\t|                T E R I M A   K A S I H                 |");
-    printf("\n\t|                                                        |");
-    printf("\n\t|         ----------SELAMAT MENONTON----------           |");
-    printf("\n\t|                                                        |");
-    printf("\n\t==========================================================\n\n");
+    printf("\n\t Terima Kasih Telah Menggunakan Sistem Pemesanan Tiket di Bioskop TI ");
+    printf("\n\t Selamat Datang Kembali! ");
+    getchar();
     exit(0);
 }
 
 int main(){
-
     intro();
-
     return 0;
 }
